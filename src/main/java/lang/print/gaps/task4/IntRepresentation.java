019ae889d75c647faab075418bdeaeeaac5cb375
Artsiom Pashun <a.pashun@syberry.com>
2023-04-12 12:10:56 +0200

    Finished module 3, task 4

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 99;
+        char h = 104;
+        char r = 114;
+        char a = 97;
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(r);
+        System.out.println(a);
     }
 }
